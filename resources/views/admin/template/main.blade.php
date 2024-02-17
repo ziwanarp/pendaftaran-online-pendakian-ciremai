a33c87c08683766b83e3a9c93df312e731cda3b6
Ziwana Rizwan Pramudia <ziwanarizwan@gmail.com>
2024-02-18 01:32:11 +0700

    fix bug view profile

--- resources/views/admin/template/main.blade.php
+++ resources/views/admin/template/main.blade.php
@@ -96,11 +96,16 @@
                             <!-- Dropdown - User Information -->
                             <div class="dropdown-menu dropdown-menu-right shadow animated--grow-in"
                                 aria-labelledby="userDropdown">
-                                <a class="dropdown-item" href="#">
-                                    <i class="fas fa-user fa-sm fa-fw mr-2 text-gray-400"></i>
-                                    Profile
-                                </a>
-                                <div class="dropdown-divider"></div>
+                                {{-- {{ dd(Request::is()); }} --}}
+                                @if ( Request::is('dashboard/user*'))
+                                @else
+                                    <a class="dropdown-item" href="user/{{ auth()->user()->username }}">
+                                        <i class="fas fa-user fa-sm fa-fw mr-2 text-gray-400"></i>
+                                        Profile
+                                    </a>
+                                    <div class="dropdown-divider"></div>  
+                                @endif
+
                                 <a class="dropdown-item" href="" data-toggle="modal" data-target="#logoutModal">
                                     <i class="fas fa-sign-out-alt fa-sm fa-fw mr-2 text-gray-400"></i>
                                     Logout
